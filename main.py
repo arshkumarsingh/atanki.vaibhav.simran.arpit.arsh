8cc76f62d632165fffd88b2c8d6996eac74e71cc
ArpitJain2005 <arpit61105@gmail.com>
2024-06-23 22:52:50 +0530

    Update main.py
    helping my homie with vocabulory

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Once upon a time")
+print("bheh chudaee gyaa hello world")
+print("hello my name is arpit jain , they call me bakchod , i dont care about other opinion as they say i am self centered ")
+f = input("guess the cuss word\n")
+cuss = ["maderchod","bhosdike","dalla","bkchod","bhehn k lund","maachuda"]
+if f == cuss[0] : 
+ print("you are a maderchod")
+elif f == cuss[1] : 
+ print("bhosdikee betteeeee")
+elif f == cuss[2] : 
+ print("amriccaa ka dallal")
+elif f == cuss[3] : 
+ print(" bkchodi se deasesed")
+elif f == cuss[4] : 
+ print("hhahaha, bhehn k lund")
+elif f == cuss[5] : 
+ print("tu jake maachuda")
+else :
+ print("tumsee naa ho payegaa glttt jawabb,tum chutiyaa ho")
